caf251760a5519d127314b3fa128118265f6becf
LightningDAISY <daisy@umr.biz>
2026-04-10 08:52:49 +0900

    停滞対応

--- app/services/shift_imports/seat_availability_recorder.rb
+++ app/services/shift_imports/seat_availability_recorder.rb
@@ -1,6 +1,6 @@
 module ShiftImports
   class SeatAvailabilityRecorder
-    KEYWORDS = [ "おせき", "お席", "💺", "🪑", "救世主", "まったり", "停滞" ].freeze
+    KEYWORDS = [ "おせき", "お席", "💺", "🪑", "救世主", "停滞", "灰色", "まったり" ].freeze
 
     def initialize(shop:, staff:, raw_text:, post_id:, post_url:, posted_at:)
       @shop = shop
